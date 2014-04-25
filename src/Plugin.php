3c40946735bf26f5e8c50aa02f1538e1c0f71ba5
elazar <tobias382@gmail.com>
2014-04-24 20:59:16 -0500

    Initial commit

---
+++ src/Plugin.php
@@ -0,0 +1,314 @@
+<?php
+/**
+ * Phergie (http://phergie.org)
+ *
+ * @link https://github.com/phergie/phergie-irc-plugin-react-usermode for the canonical source repository
+ * @copyright Copyright (c) 2008-2014 Phergie Development Team (http://phergie.org)
+ * @license http://phergie.org/license New BSD License
+ * @package Phergie\Irc\Plugin\React\UserMode
+ */
+
+namespace Phergie\Irc\Plugin\React\UserMode;
+
+use Phergie\Irc\Bot\React\AbstractPlugin;
+use Phergie\Irc\Bot\React\EventQueueInterface;
+use Phergie\Irc\ConnectionInterface;
+use Phergie\Irc\Event\EventInterface;
+use Phergie\Irc\Event\UserEventInterface;
+
+/**
+ * Plugin for monitoring and providing access to user mode information.
+ *
+ * @category Phergie
+ * @package Phergie\Irc\Plugin\React\UserMode
+ */
+class Plugin extends AbstractPlugin
+{
+    /**
+     * Mapping of connection masks to channel names to user nicknames to mode
+     * values to true values for active modes
+     *
+     * @var array
+     */
+    protected $modes = array();
+
+    /**
+     * Mapping of mode letters to corresponding nickname prefix characters
+     *
+     * @var array
+     */
+    protected $prefixes = array(
+        '~' => 'q', // owner
+        '&' => 'a', // admin
+        '@' => 'o', // op
+        '%' => 'h', // halfop
+        '+' => 'v', // voice
+    );
+
+    /**
+     * Accepts configuration.
+     *
+     * Supported keys:
+     *
+     * prefixes - optional mapping of nickname prefix characters to
+     * corresponding mode letters, used for getting initial mode information
+     * after joining a channel
+     *
+     * @param array $config
+     */
+    public function __construct(array $config = array())
+    {
+        if (isset($config['prefixes']) && is_array($config['prefixes'])) {
+            $this->prefixes = $config['prefixes'];
+        }
+    }
+
+    /**
+     * Indicates that the plugin monitors events related to user identity and
+     * presence and mode indications and changes.
+     *
+     * @return array
+     */
+    public function getSubscribedEvents()
+    {
+        return array(
+            'irc.received.mode' => 'changeUserMode',
+            'irc.received.part' => 'removeUserFromChannel',
+            'irc.received.quit' => 'removeUser',
+            'irc.received.nick' => 'changeUserNick',
+            'irc.received.rpl_namreply' => 'loadUserModes',
+        );
+    }
+
+    /**
+     * Monitors use mode changes.
+     *
+     * @param \Phergie\Irc\Event\EventInterface $event
+     * @param \Phergie\Irc\Bot\React\EventQueueInterface $queue
+     */
+    public function changeUserMode(EventInterface $event, EventQueueInterface $queue)
+    {
+        $logger = $this->getLogger();
+
+        $params = $event->getParams();
+        $logger->debug('Changing user mode', array('event' => $event));
+
+        // Disregard mode changes without both an associated channel and user
+        if (!isset($params['channel']) || !isset($params['user'])) {
+            $logger->debug('Missing channel or user, skipping');
+            return;
+        }
+
+        $connectionMask = $this->getConnectionMask($event->getConnection());
+        $channel = $params['channel'];
+        $nick = $params['user'];
+        $modes = str_split($params['mode']);
+        $operation = array_shift($modes);
+
+        $logger->debug('Extracted event data', array(
+            'connectionMask' => $connectionMask,
+            'channel' => $channel,
+            'nick' => $nick,
+            'operation' => $operation,
+            'modes' => $modes,
+        ));
+
+        foreach ($modes as $mode) {
+            switch ($operation) {
+                case '+':
+                    if (!isset($this->modes[$connectionMask])) {
+                        $this->modes[$connectionMask] = array();
+                    }
+                    if (!isset($this->modes[$connectionMask][$channel])) {
+                        $this->modes[$connectionMask][$channel] = array();
+                    }
+                    if (!isset($this->modes[$connectionMask][$channel][$nick])) {
+                        $this->modes[$connectionMask][$channel][$nick] = array();
+                    }
+                    $this->modes[$connectionMask][$channel][$nick][$mode] = true;
+                    break;
+                case '-':
+                    unset($this->modes[$connectionMask][$channel][$nick][$mode]);
+                    break;
+                default:
+                    $logger->warning('Encountered unknown operation', array(
+                        'operation' => $operation,
+                    ));
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Removes user mode data that's no longer needed.
+     *
+     * @param \Phergie\Irc\Event\EventInterface $event
+     * @param \Phergie\Irc\Bot\React\EventQueueInterface $queue
+     */
+    public function removeUserFromChannel(UserEventInterface $event, EventQueueInterface $queue)
+    {
+        $logger = $this->getLogger();
+        $logger->debug('Removing user from channel', array('event' => $event));
+        $connectionMask = $this->getConnectionMask($event->getConnection());
+        $params = $event->getParams();
+        $this->removeUserData(
+            $connectionMask,
+            explode(',', $params['channels']),
+            $event->getNick()
+        );
+    }
+
+    /**
+     * Removes user mode data that's no longer needed.
+     *
+     * @param \Phergie\Irc\Event\EventInterface $event
+     * @param \Phergie\Irc\Bot\React\EventQueueInterface $queue
+     */
+    public function removeUser(UserEventInterface $event, EventQueueInterface $queue)
+    {
+        $logger = $this->getLogger();
+        $logger->debug('Removing user from all channels', array('event' => $event));
+        $connectionMask = $this->getConnectionMask($event->getConnection());
+        $this->removeUserData(
+            $connectionMask,
+            array_keys($this->modes[$connectionMask]),
+            $event->getNick()
+        );
+    }
+
+    /**
+     * Removes mode data for a user and list of channels.
+     *
+     * @param string $connectionMask
+     * @param array $channels
+     * @param string $nick
+     */
+    protected function removeUserData($connectionMask, array $channels, $nick)
+    {
+        $logger = $this->getLogger();
+        foreach ($channels as $channel) {
+            $logger->debug('Removing user mode data', array(
+                'connectionMask' => $connectionMask,
+                'channel' => $channel,
+                'nick' => $nick,
+            ));
+            unset($this->modes[$connectionMask][$channel][$nick]);
+        }
+    }
+
+    /**
+     * Accounts for user nick changes in stored data.
+     *
+     * @param \Phergie\Irc\Event\EventInterface $event
+     * @param \Phergie\Irc\Bot\React\EventQueueInterface $queue
+     */
+    public function changeUserNick(UserEventInterface $event, EventQueueInterface $queue)
+    {
+        $logger = $this->getLogger();
+        $logger->debug('Changing user nick', array('event' => $event));
+
+        $connectionMask = $this->getConnectionMask($event->getConnection());
+        $old = $event->getNick();
+        $params = $event->getParams();
+        $new = $params['nickname'];
+
+        foreach (array_keys($this->modes[$connectionMask]) as $channel) {
+            if (!isset($this->modes[$connectionMask][$channel][$old])) {
+                continue;
+            }
+            $logger->debug('Moving user mode data', array(
+                'connectionMask' => $connectionMask,
+                'channel' => $channel,
+                'oldNick' => $old,
+                'newNick' => $new,
+            ));
+            $this->modes[$connectionMask][$channel][$new] = $this->modes[$connectionMask][$channel][$old];
+            unset($this->modes[$connectionMask][$channel][$old]);
+        }
+    }
+
+    /**
+     * Loads initial user mode data when the bot joins a channel.
+     *
+     * @param \Phergie\Irc\Event\EventInterface $event
+     * @param \Phergie\Irc\Bot\React\EventQueueInterface $queue
+     */
+    public function loadUserModes(EventInterface $event, EventQueueInterface $queue)
+    {
+        $logger = $this->getLogger();
+        $logger->debug('Gathering initial user mode data', array('event' => $event));
+
+        $connectionMask = $this->getConnectionMask($event->getConnection());
+        $params = $event->getParams();
+        $channel = ltrim(array_shift($params), '=*@');
+        $validPrefixes = implode('', array_keys($this->prefixes));
+        $pattern = '/^([' . preg_quote($validPrefixes) . ']+)(.+)$/';
+
+        foreach ($params as $fullNick) {
+            if (!preg_match($pattern, $fullNick, $match)) {
+                continue;
+            }
+            $nickPrefixes = str_split($match[1]);
+            $nick = $match[2];
+            foreach ($nickPrefixes as $prefix) {
+                $mode = $this->prefixes[$prefix];
+                $logger->debug('Recording user mode', array(
+                    'connectionMask' => $connectionMask,
+                    'channel' => $channel,
+                    'nick' => $nick,
+                    'mode' => $mode,
+                ));
+                $this->modes[$connectionMask][$channel][$nick][$mode] = true;
+            }
+        }
+    }
+
+    /**
+     * Returns whether a user has a particular mode in a particular channel.
+     *
+     * @param \Phergie\Irc\ConnectionInterface $connection
+     * @param string $channel
+     * @param string $nick
+     * @param string $mode
+     * @return boolean
+     */
+    public function userHasMode(ConnectionInterface $connection, $channel, $nick, $mode)
+    {
+        $connectionMask = $this->getConnectionMask($connection);
+        return isset($this->modes[$connectionMask][$channel][$nick][$mode]);
+    }
+
+    /**
+     * Returns a list of modes for a user in a particular channel.
+     *
+     * @param \Phergie\Irc\ConnectionInterface $connection
+     * @param string $channel
+     * @param string $nick
+     * @return array Enumerated array of mode letters or an empty array if the
+     *         user has no modes in the specified channel
+     */
+    public function getUserModes(ConnectionInterface $connection, $channel, $nick)
+    {
+        $connectionMask = $this->getConnectionMask($connection);
+        if (isset($this->modes[$connectionMask][$channel][$nick])) {
+            return array_keys($this->modes[$connectionMask][$channel][$nick]);
+        }
+        return array();
+    }
+
+    /**
+     * Returns the mask string for a given connection.
+     *
+     * @param \Phergie\Irc\ConnectionInterface $connection
+     * @return string
+     */
+    protected function getConnectionMask(ConnectionInterface $connection)
+    {
+        return strtolower(sprintf(
+            '%s!%s@%s',
+            $connection->getNickname(),
+            $connection->getUsername(),
+            $connection->getServerHostname()
+        ));
+    }
+}
---
+++ tests/Phergie/Irc/Plugin/React/UserMode/PluginTest.php
@@ -0,0 +1,382 @@
+<?php
+/**
+ * Phergie (http://phergie.org)
+ *
+ * @link https://github.com/phergie/phergie-irc-plugin-react-usermode for the canonical source repository
+ * @copyright Copyright (c) 2008-2014 Phergie Development Team (http://phergie.org)
+ * @license http://phergie.org/license New BSD License
+ * @package Phergie\Irc\Plugin\React\UserMode
+ */
+
+namespace Phergie\Irc\Plugin\React\UserMode;
+
+use Phake;
+use Phergie\Irc\Event\EventInterface;
+use Phergie\Irc\Bot\React\EventQueueInterface;
+
+/**
+ * Tests for the Plugin class.
+ *
+ * @category Phergie
+ * @package Phergie\Irc\Plugin\React\UserMode
+ */
+class PluginTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Instance of the class under test
+     *
+     * @var \Phergie\Irc\Plugin\React\UserMode\Plugin
+     */
+    protected $plugin;
+
+    /**
+     * Mock logger
+     *
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * Mock event queue
+     *
+     * @var \Phergie\Irc\Bot\React\EventQueueInterface
+     */
+    protected $queue;
+
+    /**
+     * Instantiates the class under test.
+     */
+    protected function setUp()
+    {
+        $this->plugin = new Plugin;
+
+        $this->logger = Phake::mock('\Psr\Log\LoggerInterface');
+        $this->plugin->setLogger($this->logger);
+
+        $this->queue = Phake::mock('\Phergie\Irc\Bot\React\EventQueueInterface');
+    }
+
+    /**
+     * Data provider for testChangeUserModeDisregardsEvents().
+     *
+     * @return array
+     */
+    public function dataProviderChangeUserModeDisregardsEvents()
+    {
+        $data = array();
+
+        // User without channel
+        $data[] = array(array('user' => 'nick'));
+
+        // Channel without user
+        $data[] = array(array('channel' => '#channel'));
+
+        // Neither channel nor user
+        $data[] = array(array());
+
+        return $data;
+    }
+
+    /**
+     * Tests changeUserMode() with events it will disregard.
+     *
+     * @param array $params Event parameters
+     * @dataProvider dataProviderChangeUserModeDisregardsEvents
+     */
+    public function testChangeUserModeDisregardsEvents(array $params)
+    {
+        $event = $this->getMockEvent();
+        Phake::when($event)->getParams()->thenReturn($params);
+
+        $this->plugin->changeUserMode($event, $this->queue);
+
+        Phake::verify($this->logger)->debug('Missing channel or user, skipping');
+    }
+
+    /**
+     * Returns a mock user mode event.
+     *
+     * @param string $operation
+     * @param string $channel
+     * @param string $nick
+     * @return \Phergie\Irc\Event\EventInterface
+     */
+    protected function getMockUserModeEvent($operation = '+', $channel = '#channel', $nick = 'nick')
+    {
+        $mode = 'o';
+        $params = array(
+            'channel' => $channel,
+            'user' => $nick,
+            'mode' => $operation . $mode,
+        );
+        $connection = $this->getMockConnection();
+        $event = $this->getMockEvent();
+        Phake::when($event)->getParams()->thenReturn($params);
+        Phake::when($event)->getConnection()->thenReturn($connection);
+        return $event;
+    }
+
+    /**
+     * Tests changeUserMode() adding a mode.
+     */
+    public function testChangeUserModeAddsMode()
+    {
+        $event = $this->getMockUserModeEvent();
+
+        $this->plugin->changeUserMode($event, $this->queue);
+
+        $this->assertTrue($this->plugin->userHasMode($event->getConnection(), '#channel', 'nick', 'o'));
+    }
+
+    /**
+     * Tests changeUserMode() removing a mode.
+     */
+    public function testChangeUserModeRemovesMode()
+    {
+        $this->testChangeUserModeAddsMode();
+
+        $event = $this->getMockUserModeEvent('-');
+
+        $this->plugin->changeUserMode($event, $this->queue);
+
+        $this->assertFalse($this->plugin->userHasMode($event->getConnection(), '#channel', 'nick', 'o'));
+    }
+
+    /**
+     * Tests changeUserMode() with an unknown operation.
+     */
+    public function testChangeUserModeWithUnknownOperation()
+    {
+        $operation = '~';
+        $event = $this->getMockUserModeEvent($operation);
+
+        $this->plugin->changeUserMode($event, $this->queue);
+
+        $this->assertFalse($this->plugin->userHasMode($event->getConnection(), '#channel', 'nick', 'o'));
+        Phake::verify($this->logger)->warning(
+            'Encountered unknown operation',
+            array('operation' => $operation)
+        );
+    }
+
+    /**
+     * Tests removeUserFromChannel().
+     */
+    public function testRemoveUserFromChannel()
+    {
+        $this->testChangeUserModeAddsMode();
+
+        $channel = '#channel';
+        $nick = 'nick';
+        $mode = 'o';
+        $params = array(
+            'channels' => $channel,
+        );
+        $connection = $this->getMockConnection();
+        $event = $this->getMockUserEvent();
+        Phake::when($event)->getParams()->thenReturn($params);
+        Phake::when($event)->getConnection()->thenReturn($connection);
+        Phake::when($event)->getNick()->thenReturn($nick);
+
+        $this->plugin->removeUserFromChannel($event, $this->queue);
+
+        $this->assertFalse($this->plugin->userHasMode($connection, $channel, $nick, $mode));
+    }
+
+    /**
+     * Tests removeUser().
+     */
+    public function testRemoveUser()
+    {
+        $event = $this->getMockUserModeEvent('+', '#channel1');
+        $this->plugin->changeUserMode($event, $this->queue);
+
+        $event = $this->getMockUserModeEvent('+', '#channel2');
+        $this->plugin->changeUserMode($event, $this->queue);
+
+        $channel = '#channel1,#channel2';
+        $nick = 'nick';
+        $mode = 'o';
+        $params = array(
+            'channels' => $channel,
+        );
+        $connection = $this->getMockConnection();
+        $event = $this->getMockUserEvent();
+        Phake::when($event)->getParams()->thenReturn($params);
+        Phake::when($event)->getConnection()->thenReturn($connection);
+        Phake::when($event)->getNick()->thenReturn($nick);
+
+        $this->plugin->removeUser($event, $this->queue);
+
+        $this->assertFalse($this->plugin->userHasMode($connection, '#channel1', $nick, $mode));
+        $this->assertFalse($this->plugin->userHasMode($connection, '#channel2', $nick, $mode));
+    }
+
+    /**
+     * Tests changeUserNick().
+     */
+    public function testChangeUserNick()
+    {
+        $otherChannel = '#channel1';
+        $otherNick = 'otherNick';
+        $event = $this->getMockUserModeEvent('+', $otherChannel, $otherNick);
+        $connection = $event->getConnection();
+        $this->plugin->changeUserMode($event, $this->queue);
+
+        $oldNick = 'old';
+        $newNick = 'new';
+        $channel = '#channel2';
+        $mode = 'o';
+        $event = $this->getMockUserModeEvent('+', $channel, $oldNick);
+        $this->plugin->changeUserMode($event, $this->queue);
+
+        $event = $this->getMockUserEvent();
+        $params = array('nickname' => $newNick);
+        Phake::when($event)->getParams()->thenReturn($params);
+        Phake::when($event)->getConnection()->thenReturn($connection);
+        Phake::when($event)->getNick()->thenReturn($oldNick);
+        $this->plugin->changeUserNick($event, $this->queue);
+
+        $this->assertFalse($this->plugin->userHasMode($connection, $channel, $oldNick, $mode));
+        $this->assertFalse($this->plugin->userHasMode($connection, $otherChannel, $oldNick, $mode));
+
+        $this->assertTrue($this->plugin->userHasMode($connection, $channel, $newNick, $mode));
+        $this->assertFalse($this->plugin->userHasMode($connection, $otherChannel, $newNick, $mode));
+
+        $this->assertFalse($this->plugin->userHasMode($connection, $channel, $otherNick, $mode));
+        $this->assertTrue($this->plugin->userHasMode($connection, $otherChannel, $otherNick, $mode));
+    }
+
+    /**
+     * Data provider for testLoadUserModesWithSupportedPrefixes().
+     *
+     * @return array
+     */
+    public function dataProviderLoadUserModesWithSupportedPrefixes()
+    {
+        return array(
+            array('='),
+            array('*'),
+            array('@'),
+        );
+    }
+
+    /**
+     * Tests loadUserModes() with supported prefixes.
+     *
+     * @param string $channelPrefix Channel name prefix
+     * @dataProvider dataProviderLoadUserModesWithSupportedPrefixes
+     */
+    public function testLoadUserModesWithSupportedPrefixes($channelPrefix)
+    {
+        $channel = '#channel';
+        $params = array(
+            $channelPrefix . $channel,
+            '~owner',
+            '&admin',
+            '@op',
+            '%halfop',
+            '+voice',
+            '&@multi',
+            'regular',
+        );
+        $event = $this->getMockEvent();
+        $connection = $this->getMockConnection();
+        Phake::when($event)->getParams()->thenReturn($params);
+        Phake::when($event)->getConnection()->thenReturn($connection);
+
+        $this->plugin->loadUserModes($event, $this->queue);
+
+        $this->assertSame(array('q'), $this->plugin->getUserModes($connection, $channel, 'owner'));
+        $this->assertSame(array('a'), $this->plugin->getUserModes($connection, $channel, 'admin'));
+        $this->assertSame(array('o'), $this->plugin->getUserModes($connection, $channel, 'op'));
+        $this->assertSame(array('h'), $this->plugin->getUserModes($connection, $channel, 'halfop'));
+        $this->assertSame(array('v'), $this->plugin->getUserModes($connection, $channel, 'voice'));
+        $this->assertSame(array('a', 'o'), $this->plugin->getUserModes($connection, $channel, 'multi'));
+        $this->assertEmpty($this->plugin->getUserModes($connection, $channel, 'regular'));
+    }
+
+    /**
+     * Tests loadUserModes() with an unsupported prefix.
+     */
+    public function testLoadUserModesWithUnsupportedPrefix()
+    {
+        $channel = '#channel';
+        $params = array(
+            '=' . $channel,
+            '$unsupported',
+        );
+        $event = $this->getMockEvent();
+        $connection = $this->getMockConnection();
+        Phake::when($event)->getParams()->thenReturn($params);
+        Phake::when($event)->getConnection()->thenReturn($connection);
+
+        $this->plugin->loadUserModes($event, $this->queue);
+
+        $this->assertEmpty($this->plugin->getUserModes($connection, $channel, 'unsupported'));
+    }
+
+    /**
+     * Tests loadUserModes() with an overridden prefix.
+     */
+    public function testLoadUserModesWithOverriddenPrefix()
+    {
+        $channel = '#channel';
+        $params = array(
+            '=' . $channel,
+            '$unsupported',
+        );
+        $event = $this->getMockEvent();
+        $connection = $this->getMockConnection();
+        Phake::when($event)->getParams()->thenReturn($params);
+        Phake::when($event)->getConnection()->thenReturn($connection);
+
+        $plugin = new Plugin(array('prefixes' => array('$' => 'd')));
+        $plugin->setLogger($this->logger);
+        $plugin->loadUserModes($event, $this->queue);
+
+        $this->assertSame(array('d'), $plugin->getUserModes($connection, $channel, 'unsupported'));
+    }
+
+    /**
+     * Tests that getSubscribedEvents() returns an array.
+     */
+    public function testGetSubscribedEvents()
+    {
+        $this->assertInternalType('array', $this->plugin->getSubscribedEvents());
+    }
+
+    /**
+     * Returns a mock event.
+     *
+     * @return \Phergie\Irc\Event\EventInterface
+     */
+    protected function getMockEvent()
+    {
+        return Phake::mock('\Phergie\Irc\Event\EventInterface');
+    }
+
+    /**
+     * Returns a mock user event.
+     *
+     * @return \Phergie\Irc\Event\UserEventInterface
+     */
+    protected function getMockUserEvent()
+    {
+        return Phake::mock('\Phergie\Irc\Event\UserEventInterface');
+    }
+
+    /**
+     * Returns a mock connection.
+     *
+     * @return \Phergie\Irc\Bot\React\ConnectionInterface
+     */
+    protected function getMockConnection()
+    {
+        $connection = Phake::mock('\Phergie\Irc\Bot\React\ConnectionInterface');
+        Phake::when($connection)->getNickname()->thenReturn('cnickname');
+        Phake::when($connection)->getUsername()->thenReturn('cusername');
+        Phake::when($connection)->getServerHostname()->thenReturn('chostname');
+        return $connection;
+    }
+}
